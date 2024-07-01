58cf19b43cda6ccd593814ccd19b289a1432f017
GunkoNK <126878606+GunkoNK@users.noreply.github.com>
2024-07-01 11:05:49 +0300

    docker-compose

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn src.server:app --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8443 --keyfile="cert/privkey.pem" --certfile="cert/cert.pem"
+gunicorn src.server:app --workers 8 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8443 --keyfile="cert/SSL/www.objectus.ru.key" --certfile="cert/SSL/certificate_ca.crt"
